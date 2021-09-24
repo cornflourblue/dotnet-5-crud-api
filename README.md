a0b9da6e73eefcd3329895656d4d68d7dc1ea049
Jason Watmore <cornflourblue@hotmail.com>
2021-09-24 16:29:18 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotnet-5-crud-api
+
+.NET 5.0 - CRUD API Example
+
+Documentation coming soon!
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Text.Json.Serialization;
+using WebApi.Helpers;
+using WebApi.Services;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        // add services to the DI container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>();
+            services.AddCors();
+            services.AddControllers().AddJsonOptions(x =>
+            {
+                // serialize enums as strings in api responses (e.g. Role)
+                x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+                // ignore omitted parameters on models to enable optional params (e.g. User update)
+                x.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        // configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            // global error handler
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+
+            app.UseEndpoints(x => x.MapControllers());
+        }
+    }
+}
